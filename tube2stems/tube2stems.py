ceea12724c2527cd114ba4f5a81ea1aa3266e2c2
johnwmillr <john.w.millr@gmail.com>
2020-04-15 21:28:02 -0700

    SongToStems download and split combo finally working

--- tube2stems/tube2stems.py
+++ tube2stems/tube2stems.py
@@ -1,10 +1,16 @@
 # John W. Miller
 # November 2019
 
-import youtube_dl
+# TODO: Change name to song2stems
+
+import os
 import warnings
-warnings.filterwarnings('ignore')
+
+import youtube_dl
 from spleeter.separator import Separator
+
+
+warnings.filterwarnings('ignore')
 
 
 class Logger(object):
@@ -17,21 +23,22 @@ class Logger(object):
     def error(self, msg):
         print(msg)
 
-
 def hook(d):
     if d['status'] == 'finished':
         print('Done downloading, now converting...')
+            
 
-def download_youtube_audio(url, filename=None, codec="wav"):
-    """
-    Download audio from a YouTube video.
-    Adapted from: https://www.programcreek.com/python/example/98358/youtube_dl.YoutubeDL
-    
-    :url: (str)
-    :filename: (str)
-    :code: (str)
-    """
-    def create_filename(name, video_info):
+class Downloader(object):
+
+    def __init__(self, codec=None, verbose=True):
+        self.codec = codec if codec else "mp3"
+        self.verbose = verbose
+
+    def create_filename(self, name, video_info):
+        """
+        :name: (str)
+        :video_info: (str)
+        """
         from string import punctuation
 
         root = "./{name}.%(ext)s"
@@ -42,43 +49,125 @@ def download_youtube_audio(url, filename=None, codec="wav"):
                               .replace(" ", "_"))
         return root.format(name=name), name
 
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': "",
-        'noplaylist': True,
-        'continue_dl': True,
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': codec,
-            'preferredquality': '192'}],
-        'logger': Logger(),
-        'progress_hooks': [hook],
-        'keepvideo': False}
+    def download_youtube_audio(self, url, filename=None, codec=None):
+        """
+        Download audio from a YouTube video.
+        Adapted from: https://www.programcreek.com/python/example/98358/youtube_dl.YoutubeDL
+        
+        :url: (str)
+        :filename: (str)
+        :code: (str)
+        """
+        codec = codec if codec else self.codec
 
-    try:
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.cache.remove()
-            info_dict = ydl.extract_info(url, download=False)
-            filename, name = create_filename(filename, info_dict)
-            ydl.params["outtmpl"] = filename
-            ydl.download([url])
-            return filename, name
-    except Exception as e:
-        print(f"Error while downloading: {e}")
-        return None, None
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'outtmpl': "",
+            'noplaylist': True,
+            'continue_dl': True,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': codec,
+                'preferredquality': '192'}],
+            'logger': Logger(),
+            'progress_hooks': [hook],
+            'keepvideo': False}
 
-def download_urls(urls, filenames, codec="wav"):
-    for url, filename in zip(urls, filenames):
-        download_youtube_audio(url, filename, codec)
+        try:
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.cache.remove()
+                info_dict = ydl.extract_info(url, download=False)
+                filename, name = self.create_filename(filename, info_dict)
+                ydl.params["outtmpl"] = filename
+                ydl.download([url])
+                return f'{name}.{codec}' # TODO: Don't assume the codec
+        except Exception as e:
+            print(f"Error while downloading: {e}")
+            return None, None
 
-# def split_into_stems()
+    def download_urls(self, urls, filenames, codec='mp3'):
+        """
+        :urls: (list)
+        :filenames: (list)
+        :codec: (str)
+        """
+        codec = codec if codec else self.codec
+        msg = "`urls` and `filenames` must be the same length."
+        assert len(urls) == len(filenames), msg
+        
+        # Download the audio for each URL
+        filenames = []
+        for url, filename in zip(urls, filenames):
+            filenames.append(self.download_youtube_audio(url, filename))
+        return filenames
 
 
-# Andy Shauf: 'https://www.youtube.com/watch?v=ne0VnqTHsCk'
+class Splitter(object):
+
+    def __init__(self, codec=None):
+        self.codec = codec if codec else "mp3"
+
+    def split_into_stems(self, filename, num_stems=2):
+        """
+        Split an audio track into its track stems.
+        :filename: (str)
+        :num_stems: (int) Number of stems to split track into.
+            Options are 2, 4, or 5.
+        """
+        msg = "num_stems must be 2, 4, or 5."
+        assert num_stems in [2, 4, 5], msg
+
+        # Create the separator object
+        separator = Separator(f"spleeter:{num_stems}stems")
+
+        # Separate the audio track
+        fileout = os.path.join('stems', os.path.dirname(filename))
+        separator.separate_to_file(filename,
+                                   fileout,
+                                   codec=self.codec,
+                                   filename_format='{filename}/{instrument}.{codec}')
+
+
+class SongToStems(object):
+    """
+    Downloads YouTube audio and splits the resulting file into stems.
+    """
+
+    def __init__(self, codec=None, num_stems=2, verbose=True):
+        """
+        :codec: (str) Usually either 'wav' or 'mp3'
+        :num_stems: (int) Number of stems to split track into.
+            Options are 2, 4, or 5.
+        :verbose: (bool) Toggle verbose output.
+        """
+        self.codec = codec if codec else "mp3"
+        self.num_stems = num_stems
+        self.verbose = verbose
+
+        # Create the Downloader and Splitter instances
+        self.downloader = Downloader(codec=self.codec, verbose=self.verbose)
+        self.splitter = Splitter(codec=self.codec)
+
+    def download_and_split(self, url, filename=None):
+        """
+        Downloads audio from YouTube, splits track into stems.
+        :url: (str) YouTube video URL
+        :filename: (str) Name of output file
+        """
+        # Download the audio
+        youtube_audio_file   = self.downloader.download_youtube_audio(
+            url=url,
+            filename=filename)
+    
+        # Split into stems
+        self.splitter.split_into_stems(
+            filename=youtube_audio_file,
+            num_stems=self.num_stems)
+
 
 def main():
     print('Starting main...\n')
-    videos = {"andy_shauf_thirteen_hours": "https://www.youtube.com/watch?v=ne0VnqTHsCk"}
+    videos = {"reading": "https://www.youtube.com/watch?v=-FD1K8OvVCs"}
     urls = list(videos.values())
     names = list(videos.keys())
     download_urls(urls, names)
