08628cad4298f444fa6ef9d23c1a093647058432
johnwmillr <john.w.millr@gmail.com>
2020-04-18 18:23:20 -0700

    Rename __main__.py to __cli__.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'youstem = youstem.__main__:main']
+            'youstem = youstem.__cli__:main']
     },
     classifiers=[
         'Topic :: Software Development :: Libraries',
